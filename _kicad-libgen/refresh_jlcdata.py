b1629eb1f589a29cd99ab793d89eeb6a80130871
Datenkrake <25676449+Datenkrake@users.noreply.github.com>
2023-11-12 20:34:10 +0100

    refresh jllcdata daily

---
+++ _kicad-libgen/refresh_jlcdata.py
@@ -0,0 +1,32 @@
+import sqlite3
+from jlcquery import query_jlcparts
+
+
+def main():
+    # connect to the database
+    con = sqlite3.connect("cache.sqlite3")
+    cursor = con.cursor()
+
+    # connect to the database _kicad-libgen/db.sqlite3
+    con2 = sqlite3.connect("_kicad-libgen/db.sqlite3")
+    cursor2 = con2.cursor()
+
+    # for each component in db.sqlite3 table kicadcomponent
+    cursor2.execute("SELECT * FROM kicadcomponent")
+    components = cursor2.fetchall()
+    for component in components:
+        # get the lcsc pid
+        lcsc = component['LCSC']
+        # get the jlcdata
+        jlcdata = query_jlcparts(lcsc)
+        # update component['Price'] and component['Stock'] in db.sqlite with jlcdata['stock'] and jlcdata['price']
+        if jlcdata is not None:
+            component['Price'] = jlcdata['Price']
+            component['Stock'] = jlcdata['Stock']
+            # update the database
+            cursor2.execute(f"UPDATE kicadcomponent SET Price = '{component['Price']}', Stock = '{component['Stock']}' WHERE LCSC = '{lcsc}'")
+            con2.commit()
+
+
+if __name__ == '__main__':
+    main()
