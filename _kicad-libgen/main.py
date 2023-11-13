77a516e4d210b1ad6736e56afc9304a6a3b444f0
Datenkrake <25676449+Datenkrake@users.noreply.github.com>
2023-11-13 14:31:32 +0100

    uuid gen

--- _kicad-libgen/main.py
+++ _kicad-libgen/main.py
@@ -6,6 +6,7 @@ import pprint as pp
 import base64
 import json
 import shlex
+import sqlite3
 
 from libgen import query_item
 from jlcquery import query_jlcparts
@@ -78,6 +79,17 @@ def do_the_thing(jlc_pid: str):
 
     # update the symlibtable
     update_symlibtable(lcsc_data)
+
+    con = sqlite3.connect("_kicad-libgen/db.sqlite3")
+    cursor = con.cursor()
+    # select all components with no uuid using sqlite3
+    cursor.execute("SELECT * FROM kicadcomponent WHERE uuid IS NULL")
+    values = cursor.fetchall()
+    # generate a uuid for each component with no uuid with schema uuid = f"B3D{component[0]:06x}"
+    for component in values:
+        # generate new uuid
+        cursor.execute(f"UPDATE kicadcomponent SET uuid = 'B3D{component[0]:06x}' WHERE id = {component[0]}")
+        con.commit()
 
     return kicad_component
     
