a7cb795aff00ca01861e63461e59b42c868d9b4c
Datenkrake <25676449+Datenkrake@users.noreply.github.com>
2023-11-14 17:23:48 +0100

    Update main.py

--- _kicad-libgen/main.py
+++ _kicad-libgen/main.py
@@ -108,14 +108,50 @@ def read_github_issue(repository, issue_number):
 
     try:
         issue = repo.get_issue(issue_number)
-        # issue_dict = {
-        #     "body": issue.body
-        # }
+        issue_body = issue.body
         
-        # Convert the dictionary to JSON
-        #issue_body = json.dumps(issue_dict, indent=2)
+        # parse the issue body
+        # split the issue body into lines
+        issue_body = issue_body.splitlines()
+        # remove empty lines
+        issue_body = [line for line in issue_body if line != '']
+        thing_dict = {}
+        # first line is pid
+        pid = issue_body[0]
+        thing_dict['pid'] = pid
+        # find the first line that contains "Overwrite"
+        overwrite_line = [line for line in issue_body if "Overwrite" in line][0]
+        # if overwrite_line contains [X] then overwrite is True
+        if "[X]" in overwrite_line:
+            overwrite = True
+        else:
+            overwrite = False
+        thing_dict['overwrite'] = overwrite
+        # find the first line that contains "without LCSC"
+        without_lcsc_line = [line for line in issue_body if "without LCSC" in line][0]
+        # if without_lcsc_line contains [X] then without_lcsc is True
+        if "[X]" in without_lcsc_line:
+            without_lcsc = True
+        else:
+            without_lcsc = False
+        thing_dict['without_lcsc'] = without_lcsc
+        # find the first line that contains "### Manufacturer Part Number"
+        mpn_line = [line for line in issue_body if "### Manufacturer Part Number" in line][0]
+        # the next line is the mpn
+        mpn = issue_body[issue_body.index(mpn_line)+1]
+        thing_dict['mpn'] = mpn
+        # find the first line that contains "### Manufacturer"
+        mfr_line = [line for line in issue_body if "### Manufacturer" in line][0]
+        # the next line is the mfr
+        mfr = issue_body[issue_body.index(mfr_line)+1]
+        thing_dict['mfr'] = mfr
+        # for each line that contains ":", split the line into key and value, strip spaces from value and add them to a dict called values_dict
+        for line in issue_body:
+            if ":" in line:
+                key, value = line.split(":")
+                thing_dict[key] = value.strip()
 
-        return issue.body
+        return thing_dict
 
     except Exception as e:
         print(f"Error: {e}")
@@ -141,7 +177,8 @@ if __name__ == "__main__":
 
     # Read the GitHub issue
     issue = read_github_issue(repository_name, issue_number)
-    print(issue)
+    issue_string = json.dumps(issue)
+    print(issue_string)
     # if "," in jlc_pid:
     #     # split the list into a list of jlc_pid
     #     jlc_pid = jlc_pid.split(",")
