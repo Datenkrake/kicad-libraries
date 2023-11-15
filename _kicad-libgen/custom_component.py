e570924d96fd90f58e63372f3acf38e32b5ac13f
Datenkrake <25676449+Datenkrake@users.noreply.github.com>
2023-11-15 04:14:19 +0100

    Update custom_component.py

--- _kicad-libgen/custom_component.py
+++ _kicad-libgen/custom_component.py
@@ -66,27 +66,67 @@ def update_custom_component(pid, issue_dict: dict):
     # set the values
     if issue_dict['Symbols'] is not None and issue_dict['Symbols'] != "-":
         kicad_component.Symbols = issue_dict['Symbols']+":"+issue_dict['Symbols']
-    else:
-        kicad_component.Symbols = issue_dict['Symbols']
+    if issue_dict['Symbols'] == "-":
+        kicad_component.Symbols = None
+
     if issue_dict['Footprints'] is not None and issue_dict['Footprints'] != "-":
         kicad_component.Footprints = "footprint:"+issue_dict['Footprints']
-    else:
-        kicad_component.Footprints = issue_dict['Footprints']
+    if issue_dict['Footprints'] == "-":
+        kicad_component.Footprints = None
 
-    kicad_component.MFR = issue_dict['mfr']
-    kicad_component.MPN = issue_dict['mpn']
-    kicad_component.LCSC = None
+    if issue_dict['mfr'] is not None and issue_dict['mfr'] != "-":
+        kicad_component.MFR = issue_dict['mfr']
+    if issue_dict['mfr'] == "-":
+        kicad_component.MFR = None
+
+    if issue_dict['mpn'] is not None and issue_dict['mpn'] != "-":
+        kicad_component.MPN = issue_dict['mpn']
+    if issue_dict['mpn'] == "-":
+        kicad_component.MPN = None
+
+    # kicad_component.LCSC = None
     # kicadmodel.Value = issue_dict['Value']
-    kicad_component.Datasheet = issue_dict['Datasheet']
-    kicad_component.Description = issue_dict['Description']
+    if issue_dict['Datasheet'] is not None and issue_dict['Datasheet'] != "-":
+        kicad_component.Datasheet = issue_dict['Datasheet']
+    if issue_dict['Datasheet'] == "-":
+        kicad_component.Datasheet = None
+
+    if issue_dict['Description'] is not None and issue_dict['Description'] != "-":
+        kicad_component.Description = issue_dict['Description']
+    if issue_dict['Description'] == "-":
+        kicad_component.Description = None
+
     # kicadmodel.Stock = issue_dict['Stock']
-    kicad_component.Category = issue_dict['Category']
-    kicad_component.Subcategory = issue_dict['Subcategory']
+    if issue_dict['Category'] is not None and issue_dict['Category'] != "-":
+        kicad_component.Category = issue_dict['Category']
+    if issue_dict['Category'] == "-":
+        kicad_component.Category = None
+    
+    if issue_dict['Subcategory'] is not None and issue_dict['Subcategory'] != "-":
+       kicad_component.Subcategory = issue_dict['Subcategory']
+    if issue_dict['Subcategory'] == "-":
+        kicad_component.Subcategory = None
+
     # kicadmodel.Price = issue_dict['Price']
-    kicad_component.value1 = issue_dict['value1']
-    kicad_component.value2 = issue_dict['value2']
-    kicad_component.value3 = issue_dict['value3']
-    kicad_component.value4 = issue_dict['value4']
+    if issue_dict['value1'] is not None and issue_dict['value1'] != "-":
+        kicad_component.value1 = issue_dict['value1']
+    if issue_dict['value1'] == "-":
+        kicad_component.value1 = None
+
+    if issue_dict['value2'] is not None and issue_dict['value2'] != "-":   
+        kicad_component.value2 = issue_dict['value2']
+    if issue_dict['value2'] == "-":
+        kicad_component.value2 = None
+
+    if issue_dict['value3'] is not None and issue_dict['value3'] != "-":
+        kicad_component.value3 = issue_dict['value3']
+    if issue_dict['value3'] == "-":
+        kicad_component.value3 = None
+
+    if issue_dict['value4'] is not None and issue_dict['value4'] != "-":
+        kicad_component.value4 = issue_dict['value4']
+    if issue_dict['value4'] == "-":
+        kicad_component.value4 = None
 
     if kicad_component.Symbols is not None:
         update_symlibtable(issue_dict['Symbols'])
